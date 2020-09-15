c46da5a75b6af42403e7a4e205d47c8c3a6b981b
张旭超 <2212362272@qq.com>
2020-09-15 14:32:33 +0800

    修改统一样式
    修改统一样式

--- pages/lender/mediation/mediation.js
+++ pages/lender/mediation/mediation.js
@@ -9,6 +9,10 @@ Page({
       wx.navigateTo({
         url: '/pages/borrower/authentication/authentication'
       })
+    } else if (e.currentTarget.dataset.pm === '3') {
+      wx.navigateTo({
+        url: '/pages/lender/mediator/mediator'
+      }) 
     } else if (e.currentTarget.dataset.pm === '4') {
       wx.navigateTo({
         url: '/pages/repayment/repayment-code'
